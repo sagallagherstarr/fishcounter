40ea37646415a8bce4e1c894a24d0f0b6bc35468
scott.gallagherstarr <scott.gallagherstarr@gmail.com>
2021-04-07 13:55:54 -0700

    Added directories for backend and main, and a python start file.

---
+++ backend/main.py
@@ -0,0 +1,3 @@
+#
+# main.py
+# the main file of the backend / server side of the project
---
+++ start.py
@@ -0,0 +1,8 @@
+"""Starts the tornado server and opens a browser window to the frontend"""
+
+import logging
+
+log = logging.getLogger("setup.py")
+log.setLevel(logging.DEBUG)
+
+log.debug("setup.py")
