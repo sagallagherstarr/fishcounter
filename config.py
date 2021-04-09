65e04db6cd7c805b6b79f0d77e4778d5df1bf8e4
scott.gallagherstarr <scott.gallagherstarr@gmail.com>
2021-04-09 12:31:59 -0700

    Added tornado-restful in the libs folder. Need to modify this to not depend on pycrypto, which is woefully out of date now.

--- config.py
+++ config.py
@@ -16,3 +16,21 @@ log.debug("staticDirStr is '%s'", staticDirStr)
 
 ENTRY_POINT = "/"
 # STATIC_ENTRY = "/static/"
+
+# settings.py
+# import os
+
+# BASE_DIR = os.path.dirname(__file__)
+
+debug = True
+secret_key = "******"
+
+routers_path = os.path.join(BASE_DIR, "routers")
+api_prefix = "/api"
+trailing_slash = False
+
+mysql_host = "127.0.0.1"
+mysql_port = 3306
+mysql_username = "fishcounter"
+mysql_password = "temppw1"
+mysql_dbname = "fishcounter"
